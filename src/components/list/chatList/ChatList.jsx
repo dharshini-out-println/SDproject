fba94433cb7cb7e384a9ea5af1a29c860813f751
dharshini-out-println <1vijayadharshini@gmail.com>
2024-09-22 01:36:44 +0530

    updated

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import "./chatList.css"
 
 const ChatList = () => {
+    const [addMode,setAddMode]=useState(false)
     return(
         <div className='chatList'>
             <div className="search">
@@ -8,7 +10,43 @@ const ChatList = () => {
                     <img src="/search.png" alt=""/>
                     <input type="text" placeholder="Search"/>
                 </div>
-                <img src="./plus.png" alt=""/>
+                <img src={addMode ? "./minus.png" : "./plus.png"} alt="" className="add"
+                onClick={()=>setAddMode((prev)=>!prev)}/>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+            <div className="item">
+                <img src="./avatar.png" alt=""/>
+                <div className="texts">
+                    <span>Jane Doe</span>
+                    <p>Hello</p>
+                </div>
             </div>
         </div>
     )

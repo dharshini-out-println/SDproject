aa246d97b53bc89ae3f7611eaf87a6b0e6011a72
dharshini-out-println <1vijayadharshini@gmail.com>
2024-09-21 18:54:56 +0530

    updated

--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -4,15 +4,15 @@ const Userinfo = () => {
     return(
         <div className='userInfo'>
             <div className="user">
-            <img src="./avatar.png" alt=""/>
-            <h2>John Doe</h2>
+              <img src="./avatar.png" alt=""/>
+              <h2>John Doe</h2>
             </div>
             <div className="icons">
-            <img src="./more.png" alt=""/>
-            <img src="./video.png" alt=""/>
-            <img src="./edit.png" alt=""/>
+              <img src="./more.png" alt=""/>
+              <img src="./video.png" alt=""/>
+              <img src="./edit.png" alt=""/>
+            </div>
         </div>
-     </div>
     )
 }
 
